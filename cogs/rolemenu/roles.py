374d4bcca467705531a6ff6ed475ca51b0df3d83
kahleeeb3 <powellcalebm@gmail.com>
2021-09-22 13:51:06 -0400

    new role menu

--- cogs/rolemenu/roles.py
+++ cogs/rolemenu/roles.py
@@ -11,45 +11,50 @@ class RoleReactions(commands.Cog):
 
     @commands.command()
     async def rolemenu(self,ctx):
-        # creates the embed
-        embed = menus.get_embed(1)
-        menu = await ctx.send(embed=embed)
-        # add the reactions
-        await menus.add_emojis(menu, 1)
-        # change the role menu id
-        menus.change_id(menu.id)
+        menu = await ctx.send('If you would like to be an active member please react with ✅')
+        await menu.add_reaction('✅')
+        
 
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
 
         # Gets the information from reaction
-        information = menus.get(payload)
-        user = information['user']
-        emoji= information['emoji']
-        message_id = information['message_id']
+        user = payload.member
+        channel_id = payload.channel_id
+        emoji = payload.emoji.name
 
         # checks if input is from bot
         if user.bot:
             pass
-        else:
+        elif emoji == '✅' and channel_id == 885318831688548363:
             # checks if its from the correct message id
-            if menus.check_message_id(message_id):
-                #check if reaction is ◀ or ▶
-                if emoji.name == '▶':
-                    await menus.flip_right(self.client, payload)
+                channel = self.client.get_channel(payload.channel_id)
+                msg = await channel.fetch_message(payload.message_id)
 
-                else:
-                    # get the current menu number
-                    msg = await menus.get_msg(self.client, payload)
-                    menu_num = msg.embeds[0].title[0]
-                    # find the matching role
-                    role_id = menus.get_role_id(menu_num, emoji)
-                    role = msg.guild.get_role(role_id)
-                    if role in user.roles:
-                        await user.remove_roles(role)
-                    else:
-                        await user.add_roles(role)
+                role = msg.guild.get_role(890279940090626098)
+                await user.add_roles(role)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        # Gets the information from reaction
+        channel = self.client.get_channel(payload.channel_id)
+        msg = await channel.fetch_message(payload.message_id)
+        user = await msg.guild.fetch_member(payload.user_id)
+
+        channel_id = payload.channel_id
+        emoji = payload.emoji.name
+
+        # checks if input is from bot
+        if user.bot:
+            pass
+        elif emoji == '✅' and channel_id == 885318831688548363:
+            # checks if its from the correct message id
+            role = msg.guild.get_role(890279940090626098)
+            await user.remove_roles(role)
+                
+
+                
                         
             
 def setup(client):
